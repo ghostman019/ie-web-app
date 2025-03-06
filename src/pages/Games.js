8d010e8fdaa2189b1c5685462dc84c546e443791
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-06 20:49:29 +0000

    Zz

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -6,18 +6,18 @@ export default function Games() {
       <h1 className="text-5xl font-extrabold text-center">Games</h1>
       <p className="mt-4 text-lg text-center">Explore and play our exciting games.</p>
       <div className="game-grid mt-6">
-        <div className="game-card">
-          <h2 className="text-2xl font-bold text-center">Game 1</h2>
-          <p className="mt-2 text-center">Description of Game 1.</p>
-        </div>
-        <div className="game-card">
-          <h2 className="text-2xl font-bold text-center">Game 2</h2>
-          <p className="mt-2 text-center">Description of Game 2.</p>
-        </div>
-        <div className="game-card">
+        <a href="/games/copter/copter.html" target="_blank" rel="noopener noreferrer" className="game-card mt-4 inline-block bg-green-500 text-black px-4 py-2 rounded-lg">
+          <h2 className="text-2xl font-bold text-center">IE Copter</h2>
+          <p className="mt-2 text-center">Fly through the skies in this thrilling $IE remake of the nostalgic Copter game.</p>
+        </a>
+        <a href="/games/web-pacman-main/index.html" target="_blank" rel="noopener noreferrer" className="game-card mt-4 inline-block bg-green-500 text-black px-4 py-2 rounded-lg">
+          <h2 className="text-2xl font-bold text-center">Pac-man</h2>
+          <p className="mt-2 text-center">Chomp your way through the maze, avoid the ghosts and prove your skill in this classic arcade adventure!</p>
+        </a>
+        <a href="/games/game3" target="_blank" rel="noopener noreferrer" className="game-card mt-4 inline-block bg-green-500 text-black px-4 py-2 rounded-lg">
           <h2 className="text-2xl font-bold text-center">Game 3</h2>
-          <p className="mt-2 text-center">Description of Game 3.</p>
-        </div>
+          <p className="mt-2 text-center">Coming soon.</p>
+        </a>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import MediaPlayer from "../components/MediaPlayer";
 import SwapComponent from "../components/SwapComponent";
 import WalletContextProvider from "../context/WalletContextProvider";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faInstagram, faDiscord, faTelegram } from '@fortawesome/free-brands-svg-icons';
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 import WalletTracker from "../components/WalletTracker"; // Ensure this import is correct
 import '../components/SwapComponent.css';
@@ -21,18 +21,21 @@ export default function Home() {
         <SwapComponent />
         <WalletTracker walletAddress="CWeBUhLXGyXPBvsfL99VoZnVtC4uQfUh7cW8xiMY8N73" tokenMintAddress="DfYVDWY1ELNpQ4s1CK5d7EJcgCGYw27DgQo2bFzMH6fA" /> {/* Add the wallet tracker component here */}
         <button className="mt-6 bg-green-500 text-black px-4 py-2 rounded-lg">Get Started</button>
-        <div className="mt-6 flex space-x-4">
-          <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
+        <div className="social-media-links mt-6 flex space-x-4">
+          <a href="https://x.com/OG_IE_CTO?t=Hsgzf6S7bcbHzrMfQwl_rA&s=09" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
           </a>
-          <a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <a href="https://www.instagram.com/p/DG23zB_qkno/?igsh=ZmtvcmZ3dzEwdmFp" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
           </a>
-          <a href="https://discord.com/invite/yourinvite" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faDiscord} size="2x" />
+          <a href="https://discord.gg/2UsvMaKRAD" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faDiscord} size="lg" />
           </a>
-          <a href="https://dex.yourwebsite.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faExchangeAlt} size="2x" />
+          <a href="https://t.me/IECommunityowned" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faTelegram} size="lg" />
+          </a>
+          <a href="https://dexscreener.com/solana/HU9TSBH3HsY1GFAtCNsAX2B5jCvt7D8WFR29ioL54rgn" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faExchangeAlt} size="lg" />
           </a>
         </div>
       </div>
