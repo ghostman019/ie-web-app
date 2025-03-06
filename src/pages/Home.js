f94bd652f27836b4447c0af2bd68c43b5a0a01f3
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-06 23:46:31 +0000

    uuug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,13 +14,13 @@ export default function Home() {
     <WalletContextProvider>
       <div className="home-container padding-container min-h-screen bg-gradient-to-r from-purple-800 to-pink-600 text-white flex flex-col justify-center items-center p-4">
         <h1 className="text-5xl font-extrabold text-center">Welcome to $IE</h1>
-        <p className="mt-4 text-lg text-center">The ultimate vaporwave meme coin.</p>
+        <p className="mt-4 text-lg text-center">The ultimate nostalgia meme coin.</p>
         <div className="w-full flex justify-center">
           <MediaPlayer videoSrc="/assets/music.mp4" /> {/* Ensure the video path is correct */}
         </div>
         <SwapComponent />
         <WalletTracker walletAddress="CWeBUhLXGyXPBvsfL99VoZnVtC4uQfUh7cW8xiMY8N73" tokenMintAddress="DfYVDWY1ELNpQ4s1CK5d7EJcgCGYw27DgQo2bFzMH6fA" /> {/* Add the wallet tracker component here */}
-        <button className="mt-6 bg-green-500 text-black px-4 py-2 rounded-lg">Get Started</button>
+     
         <div className="social-media-links mt-6 flex space-x-4">
           <a href="https://x.com/OG_IE_CTO?t=Hsgzf6S7bcbHzrMfQwl_rA&s=09" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
             <FontAwesomeIcon icon={faTwitter} size="lg" />
