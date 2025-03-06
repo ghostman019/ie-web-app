2253f87b5152877123d9c3711bb06a11210eb041
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-06 16:12:36 +0000

    5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,12 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <img src="/path/to/ie-logo.png" alt="IE Logo" className="navbar-logo" />
+        <img src="../assets/internet-exp-logo.png" alt="IE Logo" className="navbar-logo" />
         <span className="navbar-title">Internet Explorer</span>
       </div>
       <div className="navbar-right">
-        <img src="/path/to/back-icon.png" alt="Back" className="navbar-icon" />
-        <img src="/path/to/forward-icon.png" alt="Forward" className="navbar-icon" />
+        <img src="../assets/back.png" alt="Back" className="navbar-icon" />
+        <img src="../assets/forward.png" alt="Forward" className="navbar-icon" />
         <img src="/path/to/refresh-icon.png" alt="Refresh" className="navbar-icon" />
         <Link to="/" className="navbar-link">Home</Link>
         <Link to="/games" className="navbar-link">Games</Link>
