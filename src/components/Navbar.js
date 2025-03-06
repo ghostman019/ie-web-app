2033d9d585e6e4bb4075185ae6cd6624f7fb8636
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-06 16:58:08 +0000

    JBJ

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ export default function Navbar() {
         <span className="navbar-title">Internet Explorer</span>
       </div>
       <div className="navbar-right">
-        <img src="../assets/back.png" alt="Back" className="navbar-icon" />
-        <img src="../assets/forward.png" alt="Forward" className="navbar-icon" />
+        <img src="/public/assets/back.png" alt="Back" className="navbar-icon" />
+        <img src="/public/assets/forward.png" alt="Forward" className="navbar-icon" />
         <img src="/path/to/refresh-icon.png" alt="Refresh" className="navbar-icon" />
         <Link to="/" className="navbar-link">Home</Link>
         <Link to="/games" className="navbar-link">Games</Link>
