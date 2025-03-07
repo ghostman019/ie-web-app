ec1be2a02d3b4c64b62c51e389b39c6b135e2697
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-07 10:40:28 +0000

    jnk

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import ipfs from '../utils/IPFSClient';
+import arweave from '../utils/ArweaveClient';
 import '../styles/FileUpload.css';
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
-  const [ipfsHash, setIpfsHash] = useState('');
+  const [arweaveHash, setArweaveHash] = useState('');
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -13,11 +13,22 @@ const FileUpload = () => {
   const handleUpload = async () => {
     if (file) {
       try {
-        const added = await ipfs.add(file);
-        setIpfsHash(added.path);
-        console.log('File uploaded to IPFS with hash:', added.path);
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+          const data = e.target.result;
+          const transaction = await arweave.createTransaction({ data });
+          await arweave.transactions.sign(transaction);
+          const response = await arweave.transactions.post(transaction);
+          if (response.status === 200) {
+            setArweaveHash(transaction.id);
+            console.log('File uploaded to Arweave with hash:', transaction.id);
+          } else {
+            console.error('Error uploading file to Arweave:', response);
+          }
+        };
+        reader.readAsArrayBuffer(file);
       } catch (error) {
-        console.error('Error uploading file to IPFS:', error);
+        console.error('Error uploading file to Arweave:', error);
       }
     }
   };
@@ -27,13 +38,13 @@ const FileUpload = () => {
       <h2 className="text-2xl font-semibold mb-2">Upload File to Internet 1.5</h2>
       <input type="file" onChange={handleFileChange} className="mb-4" />
       <button onClick={handleUpload} className="bg-blue-500 text-black px-4 py-2 rounded-lg">
-        Upload to IPFS
+        Upload to Arweave
       </button>
-      {ipfsHash && (
+      {arweaveHash && (
         <div className="mt-4">
-          <p>File uploaded to IPFS with hash:</p>
-          <a href={`https://ipfs.infura.io/ipfs/${ipfsHash}`} target="_blank" rel="noopener noreferrer">
-            {ipfsHash}
+          <p>File uploaded to Arweave with hash:</p>
+          <a href={`https://arweave.net/${arweaveHash}`} target="_blank" rel="noopener noreferrer">
+            {arweaveHash}
           </a>
         </div>
       )}
--- src/utils/IPFSClient.js
+++
@@ -1,5 +0,0 @@
-import { create } from 'ipfs-http-client';
-
-const ipfs = create('https://ipfs.infura.io:5001/api/v0');
-
-export default ipfs;
