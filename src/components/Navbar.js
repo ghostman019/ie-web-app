c72f29374acdb392168af67aa7a85c26913129dc
MichaelMireku <mirekumichaelk@gmail.com>
2025-03-07 23:36:19 +0000

    kh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
 
         <Link to="/whitepaper" className="navbar-link">Whitepaper</Link>
         <Link to="/roadmap" className="navbar-link">Roadmap</Link>
-        <Link to="/internet15" className="navbar-link">Internet 1.5</Link> {/* New link to Internet 1.5 page */}
+        <Link to="/internet15" className="navbar-link">Internet 1.5 (beta)</Link> {/* New link to Internet 1.5 page */}
         <div className="search-bar-container">
           <input type="text" className="search-bar" value={tokenAddress} readOnly />
           <a href="https://raydium.io/swap/?inputMint=sol&outputMint=DfYVDWY1ELNpQ4s1CK5d7EJcgCGYw27DgQo2bFzMH6fA" target="_blank" rel="noopener noreferrer" className="go-button">Go</a>
