2e37e02b56b2a363d6c6adc68fdc3f632922c58f
Michael Mireku <mirekumichaelk@gmail.com>
2025-05-09 19:41:19 +0000

    Update Internet15.js

--- src/pages/Internet15.js
+++ src/pages/Internet15.js
@@ -5,7 +5,7 @@ import Archive from '../components/Archive';
 const Internet15 = () => {
   return (
     <div className="internet15 min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-8">Add IE Filter</h1>
+      <h1 className="text-4xl font-bold mb-8">Add $IE Filter</h1>
       <FileUpload /> {/* Include the FileUpload component */}
       <Archive /> {/* Include the Archive component */}
     </div>
